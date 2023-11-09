d6344e87186ac42af7001a015189aa1b0fffe9ad
caleb lehman <caleblehman@econ2-192-189-dhcp.int.colorado.edu>
2023-11-09 14:14:31 -0700

    added johndoe to users

--- Project Code/src/init_data/insert.sql
+++ Project Code/src/init_data/insert.sql
@@ -4,3 +4,4 @@ INSERT INTO users (username,password) VALUES ('Tom','$2a$12$nmc04NyhuWQsIuvmbw2m
 INSERT INTO users (username,password) VALUES ('Mitch','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
 INSERT INTO users (username,password) VALUES ('Trish','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
 INSERT INTO users (username,password) VALUES ('Roderigo','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
+INSERT INTO users (username,password) VALUES ('Johndoe','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
