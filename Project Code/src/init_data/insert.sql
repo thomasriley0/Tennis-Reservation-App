5c63dcbce0d77dc63118f45ecd879e9d487fa1e5
caleb lehman <caleblehman@econ2-192-189-dhcp.int.colorado.edu>
2023-11-09 14:05:43 -0700

    added us as users, username:First name, password:pass

---
+++ Project Code/src/init_data/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username,password) VALUES ('Caleb','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm'); /*password is pass*/
+INSERT INTO users (username,password) VALUES ('Jake','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
+INSERT INTO users (username,password) VALUES ('Tom','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
+INSERT INTO users (username,password) VALUES ('Mitch','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
+INSERT INTO users (username,password) VALUES ('Trish','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
+INSERT INTO users (username,password) VALUES ('Roderigo','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
