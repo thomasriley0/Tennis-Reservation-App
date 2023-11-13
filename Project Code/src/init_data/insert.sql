9e130242d73c86f730f2af02417a2a73750c661d
Rodrigo Moleiro <89442497+moliche@users.noreply.github.com>
2023-11-12 22:46:38 -0700

    Update insert.sql
    username rodrigo

--- Project Code/src/init_data/insert.sql
+++ Project Code/src/init_data/insert.sql
@@ -3,7 +3,7 @@ INSERT INTO users (username,password) VALUES ('Jake','$2a$12$nmc04NyhuWQsIuvmbw2
 INSERT INTO users (username,password) VALUES ('Tom','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
 INSERT INTO users (username,password) VALUES ('Mitch','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
 INSERT INTO users (username,password) VALUES ('Trish','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
-INSERT INTO users (username,password) VALUES ('Roderigo','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
+INSERT INTO users (username,password) VALUES ('Rodrigo','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
 INSERT INTO users (username,password) VALUES ('Johndoe','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
 INSERT INTO users (username,password) VALUES ('James','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
 INSERT INTO users (username,password) VALUES ('Theo','$2a$12$nmc04NyhuWQsIuvmbw2mAuQiuyArhSNDwwUET/hloBZ2JaNcxnuQm');
